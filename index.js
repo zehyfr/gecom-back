425bc784d84bc84cf42e6f5dec32c23ef9664faf
zehyfr <axelbrochardz@gmail.com>
2020-11-04 14:03:54 +0100

    Added start in package.json

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 const port = 3000;
+//userbalek = "{User{UserID : 12, RoleID}}"
 
 app.get("/", function(req, res){
     res.json({message : "C'est le chemin"});
@@ -10,7 +11,9 @@ app.get("/", function(req, res){
 .get("/help", function(req, res){
     res.json({message : "Dans l'espace, personne ne peut vous entendre crier"});
 })
-
+// .get("/user/:id", function(req, res){
+//     res.json(userbalek);
+// })
 .listen(port, function(){
     console.log("Listenning on http://localhost:" + port);
 });
