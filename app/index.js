a643ea01fe36238599910b055ed0dbc7e066d598
zehyfr <axelbrochardz@gmail.com>
2020-11-05 15:33:24 +0100

    adding a "working" index.js

--- app/index.js
+++ app/index.js
@@ -1,20 +1,68 @@
 const express = require("express");
 const app = express();
+//const bdd = require("./bdd");
 
-const port = 3000;
-//userbalek = "{User{UserID : 12, RoleID}}"
 
-app.get("/", function(req, res){
-    res.json({message : "C'est le chemin"});
+app.use(express.urlencoded({
+    extended : true}
+))
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/users', function(req, res){
+    res.json([
+        {
+            id : 1,
+            mail : "john.doe@gmail.com",
+            name : "John",
+            surname : "Doe",
+            role : {
+                id : 1,
+                name : "public"
+            },
+            company : null
+        },
+        {
+            id : 2,
+            mail : "jane.doe@gmail.com",
+            name : "Jane",
+            surname : "Doe",
+            role : {
+                id : 2,
+                name : "pro"
+            },
+            company : {
+                id : 1,
+                name : "JD BTP",
+                tel : "02.51.37.45.92"
+            }
+        }
+    ])
+    
 })
-
-// test
-.get("/help", function(req, res){
-    res.json({message : "Dans l'espace, personne ne peut vous entendre crier"});
+.get('/user/:id', function(req, res){
+    res.json(
+        {
+            id : 1,
+            mail : "john.doe@gmail.com",
+            name : "John",
+            surname : "Doe",
+            role : {
+                id : 1,
+                name : "public"
+            },
+            company : null
+        })
 })
-// .get("/user/:id", function(req, res){
-//     res.json(userbalek);
-// })
-.listen(port, function(){
-    console.log("Listenning on http://localhost:" + port);
+.post('/user', function(req, res){
+    res.send(req.body.name)
+})
+// .get('/projects', bdd.listProjects)
+// .get('/project/:id', bdd.getProject)
+// .post('/project', bdd.addProject)
+// .get('/comments/:id', bdd.listComments)
+// .post('/comment', bdd.addComment)
+
+.listen(PORT, function(){
+    console.log("Listenning on http://localhost:" + PORT);
 });
